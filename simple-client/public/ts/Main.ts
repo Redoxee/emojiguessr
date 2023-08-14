ea0d09bab76ce9c512b686f542950d80bc72eae0
AntonMakesGames <antonmakesgames@gmail.com>
2023-08-14 20:26:09 +0200

    trying to pivot to js modules, it doesn't work though

---
+++ simple-client/public/ts/Main.ts
@@ -0,0 +1,227 @@
+console.log("HELLO");
+import axios from "axios";
+const gameSrvUrl = "https://antonmakesgames.alwaysdata.net/";
+// const gameSrvUrl = "http://localhost:8100";
+
+let playerId = 0;
+let chefId = 0;
+let question = null;
+let refreshRate = 1000;
+let hintNumber = 0;
+let hints = [];
+let pauseRefresh = false;
+let responses = [];
+let nameTemplates : string[][] = [];
+
+let playerLabel = document.getElementById('playerLabel')!;
+let chefLabel = document.getElementById('chefLabel')!;
+let questionLabel = document.getElementById('questionLabel')!;
+let emojiPicker : HTMLElement = document.querySelector('emoji-picker')!; 
+let hintPicker = document.getElementById('hintPicker')!;
+let hintHolder = document.getElementById('hintHolder')!;
+let responsesHolder = document.getElementById('responsesHolder')!;
+let nextRoundButton = document.getElementById('nextRoundButton')!;
+
+function setVisibility(element : HTMLElement, display : boolean) {
+	element.style.display = display ? '' : 'none';
+}
+
+function RandomSeed(s : number) : ()=>number {
+	var mask = 0xffffffff;
+	var m_w  = (123456789 + s) & mask;
+	var m_z  = (987654321 - s) & mask;
+
+	return function() : number {
+		m_z = (36969 * (m_z & 65535) + (m_z >>> 16)) & mask;
+		m_w = (18000 * (m_w & 65535) + (m_w >>> 16)) & mask;
+
+		var result = ((m_z << 16) + (m_w & 65535)) >>> 0;
+		result /= 4294967296;
+		return result;
+	}
+}
+
+function getNameFromId(id : number) {
+	let name = '';
+	let rng = RandomSeed(id);
+	let firstPull = rng();
+
+	for(let i = 0; i < nameTemplates.length; ++i){
+		const part = nameTemplates[i]
+		let index = Math.floor(rng() * part.length);
+		name += part[index];
+	}
+
+	console.log(id, '->', name, '| first pull ->', firstPull);
+	return name;
+}
+
+const axiosGame = axios.create({
+	baseURL: gameSrvUrl,
+});
+
+const becomeChefButton = document.createElement('Button');
+becomeChefButton.id = "chefButton";
+becomeChefButton.textContent = "I'm the chef"; 
+becomeChefButton.addEventListener('click', (event)=>{
+	axiosGame.put(`/chefId/${playerId}`);
+});
+
+async function refresh() {
+	if(playerId && !pauseRefresh)
+	{
+		const res = await axiosGame.get(`/refresh/${playerId}`);
+		console.log(res);
+		const srvChef = Number(res.data.chefId);
+		if (res.data.question) {
+			question = res.data.question;
+			questionLabel.textContent = question;
+		}
+		else
+		{
+			questionLabel.textContent = '';
+		}
+		
+		if(srvChef !== chefId)
+		{
+			chefId = srvChef;
+			if(chefId) {
+				if(chefId === playerId) {
+					chefLabel.replaceChildren("you're the chef");
+				} else 
+				{
+					chefLabel.replaceChildren(`Chef is ${getNameFromId(chefId)}`);
+				}
+			}
+			else {
+				chefLabel.replaceChildren(becomeChefButton);
+				setVisibility(emojiPicker, false);
+			}
+		}
+
+		setVisibility(playerLabel, chefId !== 0);
+
+		hintNumber = res.data.hintNumber;
+		hints = res.data.hints;
+
+		if(chefId === playerId) {
+			if (hintNumber === 0) {
+				setVisibility(hintPicker, true);
+				setVisibility(nextRoundButton, false);
+			}
+			else {
+				setVisibility(hintPicker, false);
+				setVisibility(emojiPicker, hints.length < hintNumber)
+			
+				setVisibility(nextRoundButton, hints.length >= hintNumber);
+			}
+		}
+		else {
+			setVisibility(nextRoundButton, false);
+		}
+
+		if (hintNumber === 0) {
+			setVisibility(hintHolder, false);
+			setVisibility(responsesHolder, false);
+		}
+		else
+		{
+			setVisibility(hintHolder, true);
+
+			if (hintHolder.childNodes.length !== hintNumber) {
+				let hintItems = [];
+				for (let i = 0; i < hintNumber; ++i){
+					const hint = document.createElement('div');
+					hint.id = "hint";
+					hintItems.push(hint);
+				}
+
+				hintHolder.replaceChildren(...hintItems);
+			}
+
+			for (let i = 0; i < hintNumber; ++i) {
+				const hintItem = hintHolder.childNodes[i];
+				if (i < hints.length) {
+					let emo = document.createElement('p');
+					emo.textContent = hints[i];
+
+					hintItem.replaceWith(emo);
+				}
+				else {
+					hintItem.replaceWith("");
+				}
+			}
+
+			setVisibility(responsesHolder, playerId !== chefId);
+		}
+	}
+
+	setTimeout(refresh, refreshRate);
+}
+
+console.log("Hello module");
+window.addEventListener("load", async () => {
+	console.log("Hello loaded");
+	const res = await axiosGame.get('/');
+	console.log(res);
+	playerId = res.data.playerId;
+	
+	responses = res.data.responses;
+	nameTemplates = res.data.nameTemplates;
+	
+	playerLabel.replaceChildren(`You're ${getNameFromId(playerId)}`);
+	chefLabel.appendChild(becomeChefButton);
+
+	let responsesBtn = [];
+	for (let i= 0; i < responses.length; ++i){
+		const index = i;
+		const responseButton = document.createElement('button');
+		responseButton.textContent = responses[index];
+		responseButton.addEventListener('click',()=>{
+			axiosGame.put(`/response/${playerId}/${index}`);
+		});
+
+		responsesBtn.push(responseButton);
+	}
+
+	responsesHolder.replaceChildren(...responsesBtn);
+	refresh();
+});
+
+setVisibility(emojiPicker, false);
+setVisibility(hintPicker, false);
+let resetButton = document.getElementById("reset")!;
+resetButton.addEventListener('click',(event)=> {
+	axiosGame.put('/reset');
+});
+let pauseButton = document.getElementById('pause')!;
+pauseButton.addEventListener('click', ()=> {
+	pauseRefresh = !pauseRefresh;
+})
+
+let hintBtns = [];
+for(let i=0; i < 7; ++i)
+{
+	let index = i + 1;
+	const btn = document.createElement('button');
+	btn.textContent = '' + index;
+	btn.addEventListener('click',(event)=>  {
+		axiosGame.put(`/hintNumber/${index}`);
+	});
+	hintBtns.push(btn);
+}
+
+
+document.getElementById('hintPickerHolder')!.replaceChildren(...hintBtns);
+interface EmojiPickerEvent extends Event {
+	unicode : string
+}
+
+emojiPicker.addEventListener('emoji-click' , (event)=>{
+	const cevent = event as CustomEvent<EmojiPickerEvent>;
+	axiosGame.put(`/hint/${cevent.detail.unicode}`);
+});
+
+nextRoundButton.addEventListener('click', (event)=> {
+	axiosGame.put('/nextRound'); 
+});
