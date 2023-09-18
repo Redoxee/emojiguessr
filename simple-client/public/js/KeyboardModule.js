ca3dc6de1fca397a9d8b290663b60fdbac4b13c6
AntonMakesGames <antonmakesgames@gmail.com>
2023-09-18 13:42:05 +0200

    refactored the keyboard module into a class

--- simple-client/public/js/KeyboardModule.js
+++ simple-client/public/js/KeyboardModule.js
@@ -11,61 +11,64 @@ const layouts = {
         ["z", "x", "c", "v", "b", "n", "m", "é", "è"]
     ]
 };
-function create_keyboard_element(layout_name) {
-    const layout = layouts[layout_name] || layouts.qwerty;
-    const input_event_name = "OnCustomKeyBoardInput";
-    const kb = document.createElement("div");
-    kb.className = "keyboard";
-    const keyPressedFeedbackClass = "keyboard-key-pressed-feedback";
-    const keys = {};
-    layout.forEach(row => {
-        const rowElement = document.createElement("div");
-        rowElement.className = "keyboard-row";
-        kb.appendChild(rowElement);
-        row.forEach(key => {
-            const btn = document.createElement("button");
-            btn.textContent = key;
-            btn.className = "keyboard-key";
-            rowElement.appendChild(btn);
-            const keyName = key === "⌫" ? "backspace" : key === "↵" ? "enter" : key;
-            btn.onclick = _ => {
-                kb.dispatchEvent(new CustomEvent(input_event_name, { detail: keyName }));
-            };
-            keys[keyName] = btn;
+class KeyboardElement {
+    constructor(layoutName) {
+        const keyPressedFeedbackClass = "keyboard-key-pressed-feedback";
+        const input_event_name = "OnCustomKeyBoardInput";
+        layoutName = layoutName || "qwerty";
+        const layout = layouts[layoutName] || layouts.qwerty;
+        const keys = {};
+        this.root = document.createElement('div');
+        this.root.className = 'keyboard-element';
+        layout.forEach(row => {
+            const rowElement = document.createElement("div");
+            rowElement.className = "keyboard-row";
+            this.root.appendChild(rowElement);
+            row.forEach(key => {
+                const btn = document.createElement("button");
+                btn.textContent = key;
+                btn.className = "keyboard-key";
+                btn.setAttribute('part', 'keyboard-key');
+                rowElement.appendChild(btn);
+                const keyName = key === "⌫" ? "backspace" : key === "↵" ? "enter" : key;
+                btn.onclick = _ => {
+                    this.root.dispatchEvent(new CustomEvent(input_event_name, { detail: keyName }));
+                };
+                keys[keyName] = btn;
+            });
         });
-    });
-    const handleKeyboardEvent = (e) => {
-        if (e.isComposing || e.ctrlKey) {
-            return;
-        }
-        let pressedKey = String(e.key).toLowerCase();
-        const button = keys[pressedKey];
-        if (button && !button.classList.contains(keyPressedFeedbackClass)) {
-            keys[pressedKey].classList.add(keyPressedFeedbackClass);
-        }
-        if (pressedKey === "backspace") {
-            kb.dispatchEvent(new CustomEvent(input_event_name, { detail: pressedKey }));
-            return;
-        }
-        if (pressedKey === "enter") {
-            kb.dispatchEvent(new CustomEvent(input_event_name, { detail: pressedKey }));
-            return;
-        }
-        let found = pressedKey.match(/[a-z]|[éè]/gi);
-        if (found && found.length === 1) {
-            kb.dispatchEvent(new CustomEvent(input_event_name, { detail: pressedKey }));
-            return;
-        }
-    };
-    document.addEventListener("keydown", (e) => {
-        handleKeyboardEvent(e);
-    });
-    document.addEventListener("keyup", (e) => {
-        let pressedKey = String(e.key).toLowerCase();
-        const button = keys[pressedKey];
-        if (button && button.classList.contains(keyPressedFeedbackClass)) {
-            button.classList.remove(keyPressedFeedbackClass);
-        }
-    });
-    return kb;
+        const handleKeyboardEvent = (e) => {
+            if (e.isComposing || e.ctrlKey) {
+                return;
+            }
+            let pressedKey = String(e.key).toLowerCase();
+            const button = keys[pressedKey];
+            if (button && !button.classList.contains(keyPressedFeedbackClass)) {
+                keys[pressedKey].classList.add(keyPressedFeedbackClass);
+            }
+            if (pressedKey === "backspace") {
+                this.root.dispatchEvent(new CustomEvent(input_event_name, { detail: pressedKey }));
+                return;
+            }
+            if (pressedKey === "enter") {
+                this.root.dispatchEvent(new CustomEvent(input_event_name, { detail: pressedKey }));
+                return;
+            }
+            let found = pressedKey.match(/[a-z]|[éè]/gi);
+            if (found && found.length === 1) {
+                this.root.dispatchEvent(new CustomEvent(input_event_name, { detail: pressedKey }));
+                return;
+            }
+        };
+        document.addEventListener("keydown", (e) => {
+            handleKeyboardEvent(e);
+        });
+        document.addEventListener("keyup", (e) => {
+            let pressedKey = String(e.key).toLowerCase();
+            const button = keys[pressedKey];
+            if (button && button.classList.contains(keyPressedFeedbackClass)) {
+                button.classList.remove(keyPressedFeedbackClass);
+            }
+        });
+    }
 }
--- simple-client/public/ts/KeyboardModule.ts
+++ simple-client/public/ts/KeyboardModule.ts
@@ -1,6 +1,3 @@
-interface KeyboardElement extends HTMLDivElement {
-}
-
 interface LayoutCollection {
 	[name: string]: string[][]
 }
@@ -23,76 +20,80 @@ const layouts : LayoutCollection = {
 	]
 }
 
-function create_keyboard_element(layout_name : string) : KeyboardElement {
-	const layout = layouts[layout_name] || layouts.qwerty;
+class KeyboardElement {
+	root : HTMLDivElement;
 
-	const input_event_name = "OnCustomKeyBoardInput";
-	const kb = <KeyboardElement>document.createElement("div");
-	kb.className = "keyboard";
+	constructor(layoutName : string) {
+		const keyPressedFeedbackClass = "keyboard-key-pressed-feedback";
+		const input_event_name = "OnCustomKeyBoardInput";
+		
+		layoutName = layoutName || "qwerty";
+		const layout = layouts[layoutName] || layouts.qwerty;
+		const keys : KeyBtnCollection = {};
 
-	const keyPressedFeedbackClass = "keyboard-key-pressed-feedback";
-	const keys : KeyBtnCollection = {};
+		this.root = document.createElement('div');
+		this.root.className = 'keyboard-element';
 
-	layout.forEach(row => {
-		const rowElement = document.createElement("div");
-		rowElement.className="keyboard-row";
-		kb.appendChild(rowElement);
-		row.forEach(key => {
-			const btn = document.createElement("button");
-			btn.textContent = key;
-			btn.className = "keyboard-key";
-			rowElement.appendChild(btn);
-
-			const keyName = key === "⌫" ? "backspace" : key === "↵" ? "enter" : key;
-
-			btn.onclick=_=>{
-				kb.dispatchEvent(new CustomEvent(input_event_name,{detail:keyName}));
-			};
-			
-			keys[keyName] = btn;
+		layout.forEach(row => {
+			const rowElement = document.createElement("div");
+			rowElement.className="keyboard-row";
+			this.root.appendChild(rowElement);
+			row.forEach(key => {
+				const btn = document.createElement("button");
+				btn.textContent = key;
+				btn.className = "keyboard-key";
+				btn.setAttribute('part', 'keyboard-key');
+				rowElement.appendChild(btn);
+	
+				const keyName = key === "⌫" ? "backspace" : key === "↵" ? "enter" : key;
+	
+				btn.onclick=_=>{
+					this.root.dispatchEvent(new CustomEvent(input_event_name,{detail:keyName}));
+				};
+				
+				keys[keyName] = btn;
+			});
 		});
-	});
-
-	const handleKeyboardEvent = (e: KeyboardEvent) : void => {
-		if(e.isComposing || e.ctrlKey) {
-			return;
-		}
-
-		let pressedKey = String(e.key).toLowerCase();
-
-		const button = keys[pressedKey];
-		if (button && !button.classList.contains(keyPressedFeedbackClass)) {
-			keys[pressedKey].classList.add(keyPressedFeedbackClass);
-		}
-
-		if (pressedKey === "backspace") {
-			kb.dispatchEvent(new CustomEvent(input_event_name,{detail:pressedKey}));
-			return;
+	
+		const handleKeyboardEvent = (e: KeyboardEvent) : void => {
+			if(e.isComposing || e.ctrlKey) {
+				return;
+			}
+	
+			let pressedKey = String(e.key).toLowerCase();
+	
+			const button = keys[pressedKey];
+			if (button && !button.classList.contains(keyPressedFeedbackClass)) {
+				keys[pressedKey].classList.add(keyPressedFeedbackClass);
+			}
+	
+			if (pressedKey === "backspace") {
+				this.root.dispatchEvent(new CustomEvent(input_event_name,{detail:pressedKey}));
+				return;
+			}
+		
+			if (pressedKey === "enter") {
+				this.root.dispatchEvent(new CustomEvent(input_event_name,{detail:pressedKey}));
+				return;
+			}
+		
+			let found = pressedKey.match(/[a-z]|[éè]/gi)
+			if (found && found.length === 1) {
+				this.root.dispatchEvent(new CustomEvent(input_event_name,{detail:pressedKey}));
+				return;
+			}
 		}
 	
-		if (pressedKey === "enter") {
-			kb.dispatchEvent(new CustomEvent(input_event_name,{detail:pressedKey}));
-			return;
-		}
+		document.addEventListener("keydown", (e) => {
+			handleKeyboardEvent(e);
+		});
 	
-		let found = pressedKey.match(/[a-z]|[éè]/gi)
-		if (found && found.length === 1) {
-			kb.dispatchEvent(new CustomEvent(input_event_name,{detail:pressedKey}));
-			return;
-		}
+		document.addEventListener("keyup", (e)=>{
+			let pressedKey = String(e.key).toLowerCase();
+			const button = keys[pressedKey];
+			if (button && button.classList.contains(keyPressedFeedbackClass)) {
+				button.classList.remove(keyPressedFeedbackClass);
+			}
+		});
 	}
-
-	document.addEventListener("keydown", (e) => {
-		handleKeyboardEvent(e);
-	});
-
-	document.addEventListener("keyup", (e)=>{
-		let pressedKey = String(e.key).toLowerCase();
-		const button = keys[pressedKey];
-		if (button && button.classList.contains(keyPressedFeedbackClass)) {
-			button.classList.remove(keyPressedFeedbackClass);
-		}
-	});
-	
-	return kb;
 }
